c86b99286e80c2c1e7a30cc6b08f64ea3000b0cc
sequix <sequix@163.com>
2018-05-14 14:07:32 +0800

    1st commit

---
+++ tgrml.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# 2018/05/12
+# remove a installed .tar.gz package
+
+METADIR="$HOME/.tgmeta"
+
+test -d "$METADIR" || {
+    echo "tgrml: can't access meta folder $METADIR" 1>&2
+    exit 1
+}
+
+pkg_md5=$(md5sum "$1" | awk '{ print $1 }')
+meta_file="$METADIR/$pkg_md5"
+
+test -f "$meta_file" || {
+    echo "tgrml: can't access meta file $meta_file" 1>&2
+    exit 1
+}
+
+xargs -a "$meta_file" rm -rf
+rm -f "$meta_file"
---
+++ tgset.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# 2018/05/12
+# install a .tar.gz package.
+
+METADIR="$HOME/.tgmeta"
+test -e "$METADIR" || mkdir "$METADIR"
+
+test -d "$METADIR" || {
+    echo "tgset: can't access meta folder $METADIR" 1>&2
+    exit 1
+}
+
+prefix="${2-.}"
+pkg_md5=$(md5sum "$1" | awk '{ print $1 }')
+meta_file="$METADIR/$pkg_md5"
+
+echo -n >$meta_file
+
+test -e "$prefix" || {
+    mkdir -p "$prefix"
+    readlink -m "$prefix" >>$meta_file
+}
+
+tar -tf "$1" \
+    | sed "s/^/$prefix\//g" \
+    | xargs -d '\n' -n 1 readlink -m >>$meta_file
+
+tar -xf "$1" -C "$prefix"
